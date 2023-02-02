55d54540515e2f04fe6100a2c3f2c37e1d719b4e
Cameron Leconte <cameron.leconte@aquilapp.com>
2023-02-02 09:29:13 +0100

    Home page

---
+++ Front/arosaje/src/components/SearchComponent.vue
@@ -0,0 +1,101 @@
+<script setup>
+</script>
+
+<template>
+  <div class="SearchBar">
+      <h3>Trouver des plantes à proximité</h3>
+
+      <div class="buttons">
+        <select>
+          <option value="France">FRANCE</option>
+          <option value="Allemagne">ANGLETERRE</option>
+          <option value="Espagne">ESAPGNE</option>
+          <option value="Portugal">PORTUGAL</option>
+          <option value="Belgique">BELGIQUE</option>
+          <option value="Allemagne">ALLEMAGNE</option>
+          <option value="Italie">ITALIE</option>
+        </select>
+
+        <input type="text" placeholder="Entrez la ville">
+
+        <button>Chercher des plantes</button>
+      </div>
+    </div>
+</template>
+
+<style>
+.SearchBar {
+  max-width: 970px;
+  width: 100%;
+   background: #FFFFFF;
+  box-shadow: 0px 0px 70px rgba(19, 59, 83, 0.15);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+}
+
+h3 {
+  margin: 5px;
+}
+
+select {
+  max-width: 340px;
+  width: 100%;
+  height: 60px;
+  background: #FFFFFF;
+  border: 1px solid #060825;
+  border-radius: 5px;
+  font-family: 'Montserrat';
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #060825;
+  padding: 15px;
+  margin: 5px;
+}
+
+input {
+  max-width: 340px;
+  min-width: 150px;
+  width: 100%;
+  height: 60px;
+  background: #FFFFFF;
+  border: 1px solid #060825;
+  border-radius: 5px;
+  font-family: 'Montserrat';
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #060825;
+  padding: 15px;
+  margin: 5px;
+}
+
+button {
+  max-width: 210px;
+  width: 100%;
+  height: 60px;
+  background: #060825;
+  box-shadow: 0px 15px 35px rgba(6, 8, 37, 0.2);
+  border-radius: 5px;
+  font-family: 'Montserrat';
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #FFFFFF;
+  margin: 5px;
+}
+
+@media screen and (max-width: 1024px) {
+  .buttons {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
--- Front/arosaje/src/views/HomeView.vue
+++ Front/arosaje/src/views/HomeView.vue
@@ -1,9 +1,90 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import SearchComponent from '../components/SearchComponent.vue'
 </script>
 
 <template>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
   <main>
-    <TheWelcome />
+    <div class="box">
+      <div class="background"></div>
+
+      <div class="head">
+        <h2>Les</h2>
+        <h1>Fleurs</h1>
+        <h2>C'est mieux quand ça sent la rose ...</h2>
+
+        <p>Utiliser A'rosa-je afin de les entretenir</p>
+
+        <button class="explore">Commencer à explorer</button>
+      </div>
+      <SearchComponent />
+    </div>
   </main>
 </template>
+
+<style>
+.box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.background {
+  position: relative;
+  max-width: 422px;
+  width: 100%;
+  height: 422px;
+  background: #C5FFDC;
+  filter: blur(125px);
+  opacity: 0.25;
+}
+
+.head {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 40px;
+}
+
+h1 {
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 72px;
+  line-height: 88px;
+  color: #060825;
+}
+
+h2 {
+  font-family: 'Montserrat';
+  font-size: 27px;
+  line-height: 33px;
+  color: #060825;
+}
+
+p {
+  font-family: 'Montserrat';
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #777777;
+  margin: 10px;
+}
+
+.explore {
+  max-width: 210px;
+  width: 100%;
+  height: 60px;
+  background: #060825;
+  box-shadow: 0px 15px 35px rgba(6, 8, 37, 0.2);
+  border-radius: 30px;
+  font-family: 'Montserrat';
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  color: #FAFAFA;
+  cursor: pointer;
+  margin: 10px;
+}
+</style>
