723147bfd48a4845194103ee855be415715dfa50
elouanb7 <elouanb7@gmail.com>
2023-02-03 08:38:34 +0100

    Added id and redirection on detail for a plant

--- Front/arosaje/src/views/DetailPlanteView.vue
+++ Front/arosaje/src/views/DetailPlanteView.vue
@@ -3,15 +3,44 @@
     <div class="background"></div>
 
     <div class="plantstitle">
-      <h2><i>nom latin</i></h2>
-      <h1>nom commun</h1>
+      <h2>
+        <i>{{ plant.latinName }}</i>
+      </h2>
+      <h1>{{ plant.name }}</h1>
+      {{ plantId }}
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const PLANTS_API_BASE_URL = "http://localhost:8080/api/plants";
+
 export default {
   name: "DetailPlanteView",
+  props: ["id"],
+  components: {},
+  data() {
+    return {
+      plant: [],
+    };
+  },
+  methods: {
+    getPlant() {
+      axios.get(PLANTS_API_BASE_URL + "/" + this.plantId).then((response) => {
+        this.plant = response.data;
+      });
+    },
+  },
+  computed: {
+    plantId() {
+      return this.$route.params.id;
+    },
+  },
+  created() {
+    this.getPlant();
+  },
 };
 </script>
 
