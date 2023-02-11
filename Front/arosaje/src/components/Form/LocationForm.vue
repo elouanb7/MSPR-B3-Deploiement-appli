feec20cea83a0933913fb76176402a2955615d41
Elouan BESSETTES <elouanb7@gmail.com>
2023-02-11 17:07:28 +0100

    Edits

--- Front/arosaje/src/components/Form/LocationForm.vue
+++ Front/arosaje/src/components/Form/LocationForm.vue
@@ -1,9 +1,46 @@
-<template></template>
+<template>
+  <form @submit="validateAndSubmit">
+    Localisation
+    <fieldset class="form-group">
+      <label>Ville</label>
+      <input type="text" class="form-control" v-model="city" />
+    </fieldset>
+    <fieldset class="form-group">
+      <label>Pays</label>
+      <input type="text" class="form-control" v-model="country" />
+    </fieldset>
+    <fieldset class="form-group">
+      <label>Rue</label>
+      <input type="date" class="form-control" v-model="street" />
+    </fieldset>
+    <fieldset class="form-group">
+      <label>Code Postal</label>
+      <input type="date" class="form-control" v-model="zipCode" />
+    </fieldset>
+  </form>
+</template>
 
 <script>
 export default {
-  name: "LocationForm",
+  name: "LocationForm.vue",
+  data() {
+    return {
+      location: null,
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+fieldset {
+  border: none;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #515151;
+}
+
+input {
+  margin: 10px;
+}
+</style>
--- Front/arosaje/src/router/index.js
+++ Front/arosaje/src/router/index.js
@@ -9,16 +9,6 @@ const router = createRouter({
       component: () => import("../views/HomeView.vue"),
     },
     {
-      path: "/carte",
-      name: "carte",
-      component: () => import("../views/CarteView.vue"),
-    },
-    {
-      path: "/plantes",
-      name: "plantes",
-      component: () => import("../views/PlantesView.vue"),
-    },
-    {
       path: "/connexion",
       name: "connexion",
       component: () => import("../views/ConnexionView.vue"),
@@ -27,6 +17,11 @@ const router = createRouter({
       path: "/inscription",
       name: "inscription",
       component: () => import("../views/InscriptionView.vue"),
+    },
+    {
+      path: "/plantes",
+      name: "plantes",
+      component: () => import("../views/PlantesView.vue"),
     },
     {
       path: "/plante/:id/detail",
@@ -39,9 +34,19 @@ const router = createRouter({
       component: () => import("../components/Form/PlantForm.vue"),
     },
     {
+      path: "/carte",
+      name: "carte",
+      component: () => import("../views/CarteView.vue"),
+    },
+    {
       path: "/demande/:id/detail",
       name: "demande",
       component: () => import("../views/DetailDemandeView.vue"),
+    },
+    {
+      path: "/demande/ajout",
+      name: "addAsk",
+      component: () => import("../components/Form/AskForm.vue"),
     },
   ],
 });
