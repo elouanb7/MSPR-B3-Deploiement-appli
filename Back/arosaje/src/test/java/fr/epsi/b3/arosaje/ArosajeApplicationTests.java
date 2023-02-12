50f565c00a171ec9627ac1c94a1331dfdfcd8607
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-02-12 20:52:43 +0100

    ajout des fichiers pour la mise en conteneur du back

--- Back/arosaje/src/test/java/fr/epsi/b3/arosaje/ArosajeApplicationTests.java
+++ Back/arosaje/src/test/java/fr/epsi/b3/arosaje/ArosajeApplicationTests.java
@@ -1,13 +1,13 @@
-package fr.epsi.b3.arosaje;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ArosajeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package fr.epsi.b3.arosaje;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ArosajeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
